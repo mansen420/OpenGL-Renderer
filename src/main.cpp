7d1eabd1bcbee659678617a9a10b7cdbf8cf791f
Mansen <husenalasady21@gmail.com>
2024-02-07 22:59:28 +0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,18 @@
 #include "renderer.h"
 
 //planned features :
-//1. input handling (1/2)
-//2. GUI elements (1/2)
+//1. input handling (3/4)
+//2. GUI elements (3/4)
 //3. Import feature (1/2)
 //4. Screen space processing (x)
 //5. shader pre-processor 
 //6. shader post-processor 
 //7. center and rescale objects on load
-//8. have default objects (cube or sphere) for experimenting with shaders
+//8. have default objects (cube or sphere) for experimenting with shaders (3/4)
 //9. allow editing of object physical attributes on gui 
-//10. allow positioning of viewport inside renderport
-//11. write manual
-//12. display GL state information on gui
+//10. allow positioning of viewport inside renderport ()
+//11. write manual ()
+//12. display GL state information on gui ()
 //13. make camera system
 //we need abstraction for (1/2)shaders, ()uniforms, (x)drawing
 int main()
