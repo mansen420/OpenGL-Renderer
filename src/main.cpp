37b745b473a6193a76c9da514e7817951907374d
Mansen <husenalasady21@gmail.com>
2024-02-19 22:52:37 +0300

    Update main()

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@
 //7. center and rescale objects on load ()
 //8. have default objects (cube or sphere) for experimenting with shaders (x)
 //9. allow editing of object physical attributes on gui () 
-//10. allow positioning of viewport inside renderport ()
+//10. allow positioning of viewport inside renderport (3/4)
 //11. write manual ()
 //12. display GL state information on gui ()
 //13. make camera system
 //14. display render stats and read-only info on gui ()
-//15. separate engine interface from gui engine reflection ()
+//15. separate engine interface from gui engine reflection (1/2)
 //we need abstraction for (1/2)shaders, ()uniforms, (x)drawing
 int main()
 {   
