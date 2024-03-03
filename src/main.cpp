995a54a71c2efe223038719995f2d2c0b519f12a
Mansen <husenalasady21@gmail.com>
2024-03-03 09:39:00 +0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,20 @@
 //planned features :
 //1. input handling (x)
 //2. GUI elements (x)
-//3. Import feature (1/2)
+//3. Import feature (x)
 //4. Screen space processing (x)
 //5. shader pre-processor (probably not) 
 //6. shader post-processor (x)
-//7. center and rescale objects on load ()
+//7. center and rescale objects on load (1/2)
 //8. have default objects (cube or sphere) for experimenting with shaders (x)
-//9. allow editing of object physical attributes on gui () 
+//9. allow editing of object physical attributes on gui (1/2) 
 //10. allow positioning of viewport inside renderport (3/4)
 //11. write manual ()
 //12. display GL state information on gui ()
 //13. make camera system
 //14. display render stats and read-only info on gui ()
-//15. separate engine interface from gui engine reflection (1/2)
+//15. separate engine interface from gui engine reflection (mostly yes)
+//16. rework object module ()
 //we need abstraction for (1/2)shaders, ()uniforms, (x)drawing
 int main()
 {   
