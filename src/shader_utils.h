adbb86e218d61468dd669d19d869a8dd7e29450f
Mansen <husenalasady21@gmail.com>
2024-03-03 09:39:52 +0300

    Remove old functions

--- src/shader_utils.h
+++ src/shader_utils.h
@@ -2,16 +2,6 @@
 #include "engine_interface.h"
 #include <vector>
 
-//reads file into file_contents_holder
-//WARNING : caller must ensure that file_contents_holder is properly delete[]`d.
-bool readShaderFile(const char* file_path, char* &file_contents_holder);
-bool compileShader(renderer::shader_type_option shader, unsigned int &shader_id, const char* const &shader_source);
-bool compileShaderFromPath(renderer::shader_type_option shader, unsigned int &shader_id, const char* file_path);
-bool linkShaders(unsigned int &program_id, const unsigned int& vertex_shader_id, const unsigned int& fragment_shader_id);
-//compiles and links shaders into program_id.
-//Be warned that shader IDs will be inaccessable.
-bool makeShaderProgram(const char* vertex_shader_path, const char* fragment_shader_path, unsigned int &program_id);
-
 namespace renderer
 {
     namespace shader_manager
